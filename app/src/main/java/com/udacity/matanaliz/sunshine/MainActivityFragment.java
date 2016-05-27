dc4d569634d6e1e75097be7efe64bd1548be5e92
Oleksandr Kaspruk <ol.kaspruk@gmail.com>
2016-05-27 12:42:37 +0300

    Added ListView adapted.
    Added array adapter on list view item. Filled with forecast dummy data.

--- app/src/main/java/com/udacity/matanaliz/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/udacity/matanaliz/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,15 +25,33 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        String [] itemsArray = {
+        String [] forecastArray = {
                 "Today - Rain - 22",
                 "Tomorrow - Rain - 21",
                 "Sunday - Rain - 20",
-                "Monday - Rain - 21"
+                "Monday - Rain - 21",
+                "Tuesday - Rain - 21",
+                "Wednesday - Rain - 21",
+                "Thursday - Rain - 21",
+                "Friday - Sunny - 23"
         };
 
         List<String> itemList = new ArrayList<String>(
-                Arrays.asList(itemsArray));
+                Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> forecastAdapter =
+                new ArrayAdapter<String>(
+                        // Context of the Activity
+                        getActivity(),
+                        // Name of layout file
+                        R.layout.list_item_forecast,
+                        // Id of view inside of layout xml file
+                        R.id.list_item_forecast_textview,
+                        // Data
+                        forecastArray);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
 
         return rootView;
     }
